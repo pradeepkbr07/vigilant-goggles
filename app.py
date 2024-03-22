551c99aed0cd80911c65d65fc8f1052463b1da19
pradeepkbr07 <164230792+pradeepkbr07@users.noreply.github.com>
2024-03-22 11:47:55 +0530

    Initial commit

---
+++ app.py
@@ -0,0 +1,6 @@
+# Welcome to VIKTOR!
+# 
+# First time using VIKTOR?
+#   Keep this window open while we are preparing your
+#   codespace. You can now navigate back to your VIKTOR
+#   environment to complete the onboarding process.
